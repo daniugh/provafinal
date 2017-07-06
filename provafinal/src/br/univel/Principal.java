234a968736e3fc3a2ab697be4687226bb83497e5
Daniela <Daniela@DESKTOP-11EHIQU>
2017-07-06 20:29:39 -0300

    .

---
+++ provafinal/src/br/univel/Principal.java
@@ -0,0 +1,19 @@
+package br.univel;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Integer codigo = 169456;
+		Ex01.execute(codigo);
+		Ex02.execute(codigo);
+		Ex03.execute(codigo);
+		Ex04.execute(codigo);
+		Ex05.execute(codigo);
+		Ex06.execute(codigo);
+		Ex07.execute(codigo);
+		Ex08.execute(codigo);
+		Ex09.execute(codigo);
+		Ex10.execute(codigo);
+	}
+
+}
